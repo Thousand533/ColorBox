2e48f79a45b02731ac0130d58c8009c92a73467e
YiChien <j04566704@gmail.com>
2024-09-01 10:38:09 +0800

    Update README, clean App.jsx Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import ColorBox from './ColorBox'
 import ColorBoxes from './ColorBoxes'
 
 const colors = [
